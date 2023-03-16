4ba980138d577b44c88c5a7233a5c0a92f9c9715
Tiago Gomes <tidgomes@gmail.com>
2023-03-16 08:17:43 -0300

    understanding the props object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 301.55, date: new Date(2021, 3, 16) },
+    { title: 'Food', amount: 30, date: new Date(2021, 3, 18) }
+  ]
+
   return (
     <div className='App'>
       <h1>Olá Tiago!</h1>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        date={expenses[0].date}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        date={expenses[1].date}
+        amount={expenses[1].amount}
+      />
     </div>
   )
 }
