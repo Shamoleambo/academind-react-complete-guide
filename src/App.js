d4da28ab1a00530af135f21c0cccf1099aa8cf4d
Tiago Gomes <tidgomes@gmail.com>
2023-03-17 08:07:14 -0300

    create new dummy data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import NewExpense from './components/NewExpense/NewExpense'
 const App = () => {
   const expenses = [
     { title: 'Car Insurance', amount: 301.55, date: new Date(2021, 2, 16) },
-    { title: 'Food', amount: 30, date: new Date(2021, 2, 18) }
+    { title: 'Morgage', amount: 3550.56, date: new Date(2021, 2, 18) },
+    { title: 'Travel', amount: 1231.21, date: new Date(2020, 5, 10) },
+    { title: 'New Clothes', amount: 398.8, date: new Date(2020, 10, 1) },
+    { title: 'Party Expenses', amount: 30, date: new Date(2019, 11, 18) }
   ]
 
   const addExpenseHandler = expense => {
