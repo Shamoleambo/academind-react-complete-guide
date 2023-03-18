2aa4758a737e4097b2f1743bf24754b28323f7fa
Tiago Gomes <tidgomes@gmail.com>
2023-03-18 12:53:32 -0300

    use state for controlling the expenses and delete dummy data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
+import { useState } from 'react'
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense'
 
 const App = () => {
-  const expenses = [
-    { title: 'Car Insurance', amount: 301.55, date: new Date(2021, 2, 16) },
-    { title: 'Morgage', amount: 3550.56, date: new Date(2021, 2, 18) },
-    { title: 'Travel', amount: 1231.21, date: new Date(2020, 5, 10) },
-    { title: 'New Clothes', amount: 398.8, date: new Date(2020, 10, 1) },
-    { title: 'Party Expenses', amount: 30, date: new Date(2019, 11, 18) }
-  ]
+  const [expenses, setExpenses] = useState([])
 
   const addExpenseHandler = expense => {
-    console.log('expense ==>', expense)
+    setExpenses(prevState => [...prevState, expense])
   }
 
   return (
