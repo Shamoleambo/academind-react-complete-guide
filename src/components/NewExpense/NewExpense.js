4b050cb072f7945326060952abcdd4fae39e4f03
Tiago Gomes <tidgomes@gmail.com>
2023-03-19 13:28:35 -0300

    refactor NewExpense to have a button that controls the form display

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,14 +1,27 @@
+import { useState } from 'react'
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
 const NewExpense = props => {
+  const [newExpenseForm, setNewExpenseForm] = useState(false)
+
   const saveExpenseDataHandler = inputExpenseData => {
     const expenseData = { ...inputExpenseData, id: Math.random().toString() }
     props.onAddExpense(expenseData)
   }
+  const displayForm = () => {
+    setNewExpenseForm(prevState => !prevState)
+  }
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {newExpenseForm ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          closeNewExpenseForm={displayForm}
+        />
+      ) : (
+        <button onClick={displayForm}>Add New Expense</button>
+      )}
     </div>
   )
 }
